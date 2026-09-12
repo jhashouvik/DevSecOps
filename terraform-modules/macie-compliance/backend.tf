79c61c277724e4ae1694588b075d80fd40e802b2
jhashouvik <jhashouvik@gmail.com>
2026-09-12 23:19:57 +0530

    Bucket path correction

--- terraform-modules/macie-compliance/backend.tf
+++ terraform-modules/macie-compliance/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "wezvatech-2026-tfstate"
+    bucket         = "wezvatech-s3-2027-tfstate"
     key            = "product-name/envs/macie.tfstate"
     region         = "ap-south-1"
     encrypt        = true
